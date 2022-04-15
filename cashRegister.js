d6f7be7fa47a41f37d556477593256e79a81fb39
lucasplr <lucasriffel55@gmail.com>
2022-04-15 19:38:39 -0300

    update

--- cashRegister.js
+++ cashRegister.js
@@ -2,30 +2,33 @@ function checkCashRegister(price, cash, cid) {
   let rest = cash - price
   let sum = 0
   let change = []
+  let status = "OPEN"
   let prices = [
-    ["ONE HUNDRED", 100],
-    ["TWENTY", 20],
-    ["TEN", 10],
-    ["FIVE", 5],
-    ["ONE", 1],
-    ["QUARTER", 0.25],
-    ["DIME", 0.1],
+    ["PENNY", 0.01],
     ["NICKEL", 0.05],
-    ["PENNY", 0.01]
+    ["DIME", 0.1],
+    ["QUARTER", 0.25],
+    ["ONE", 1],
+    ["FIVE", 5],
+    ["TEN", 10],
+    ["TWENTY", 20],
+    ["ONE HUNDRED", 100]
   ]
   for (let i = 0; i < cid.length; i++){
-    if (rest >= cid[i][1])
+    if (rest >= prices[i][1] )
     sum += cid[i][1]
   }
   if (rest > sum){
     return {status: "INSUFFICIENT_FUNDS", change}
   }
   for (let i = 0; i < prices.length; i++){
-    if (rest >= prices[i][1] ){
-      return [prices[i][0], rest]
+    if (rest >= prices[i][1] && cid[i][1] >= rest){
+      change.push(prices[i])
+      rest -= prices[i][1]
+      cid[i][1] -= prices[i][1]
     }
   }
-  return change;
+  return {status, change};
 }
 
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
