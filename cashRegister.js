9e6650a70d1b96bc7eb84f1a074159ca5528c714
lucasplr <lucasriffel55@gmail.com>
2022-04-16 21:24:05 -0300

    finished

--- cashRegister.js
+++ cashRegister.js
@@ -1,40 +1,50 @@
 function checkCashRegister(price, cash, cid) {
+  let newA = [...cid].reverse()
   let rest = cash - price
   let sum = 0
   let change = []
   let status = "OPEN"
   let prices = [
-    ["PENNY", 0.01],
-    ["NICKEL", 0.05],
-    ["DIME", 0.1],
-    ["QUARTER", 0.25],
-    ["ONE", 1],
-    ["FIVE", 5],
-    ["TEN", 10],
+    ["ONE HUNDRED", 100],
     ["TWENTY", 20],
-    ["ONE HUNDRED", 100]
+    ["TEN", 10],
+    ["FIVE", 5],
+    ["ONE", 1],
+    ["QUARTER", 0.25],
+    ["DIME", 0.1],
+    ["NICKEL", 0.05],
+    ["PENNY", 0.01]
   ]
-  for (let i = 0; i < cid.length; i++){
+  for (let i = 0; i < newA.length; i++){
     if (rest >= prices[i][1] )
-    sum += cid[i][1]
+    sum += newA[i][1]
   }
   if (rest > sum){
     return {status: "INSUFFICIENT_FUNDS", change}
   }
+  if( rest === sum) {
+    status = "CLOSED"
+    return {status, change: cid}
+  }
+  
   for (let i = 0; i < prices.length; i++){
-    if (rest >= prices[i][1] && cid[i][1] >= rest){
-      change.push(prices[i])
+    let tempo = [prices[i][0], 0]
+    while (rest >= prices[i][1] && newA[i][1] > 0){
+      tempo[1] += prices[i][1]
+      newA[i][1] -= prices[i][1]
       rest -= prices[i][1]
-      cid[i][1] -= prices[i][1]
+      rest = rest.toFixed(2)
+    }
+    if (tempo[1] > 0){
+      change.push(tempo)
     }
   }
   return {status, change};
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 
 /*
-To finish later on
-
+Finished, i have been trying  and one of the answers do not correspond. So i did a search, and figured out that i need a new var inside the loop, in that case "tempo", that have the elements of the while loop, and when the while loop end, push inside the change array.
+Later i will redo, to accomplish a cleaner resolve.
 */
